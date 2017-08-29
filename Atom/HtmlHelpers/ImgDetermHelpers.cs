0fdabc4886e691472c62d06038484e05d79106db
sonne.mobstyle <sonne.mobstyle@gmail.com>
2017-08-29 04:50:33 +0300

    obnova

--- Atom/HtmlHelpers/ImgDetermHelpers.cs
+++ Atom/HtmlHelpers/ImgDetermHelpers.cs
@@ -9,8 +9,9 @@ namespace Atom.HtmlHelpers
 {
     public static class ImgDetermHelpers
     {
-        private static string reflectGetAttr(Object obj)
+        private static string reflectGetAttr(Object obj)//осуществляет поддержку передачи аттрибутов тега в параметре изпользуя анонимный тип
         {
+            //принимает анонимный тип
             Type type = obj.GetType();
             PropertyInfo[] properties = type.GetProperties();
             string output = "";
@@ -18,14 +19,16 @@ namespace Atom.HtmlHelpers
             {
                 output += p.Name + "='" + p.GetValue(obj).ToString() + "' ";
             }
-            return output;
+            return output;//выводит строку с перечнем аттрибутов и значений
         }
         public static string ImgDeterm(string img, Object attributes=null, string location= "/Content/img/", string type="jpg")
         {
+            //выводит строку с тегом img
             return("<img src='"+location+img+"."+type+"' " + ((attributes!=null)?reflectGetAttr(attributes):"")+ "/>");
         }
         public static MvcHtmlString TapeDeterm(this HtmlHelper html, string imgIn)
         {
+            //выводит тег img для blog/index
             return MvcHtmlString.Create(ImgDeterm(imgIn, new {@class="media-object", width="200" }));
         }
     }
